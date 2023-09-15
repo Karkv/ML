c6e45161acd1327577466eb0dd2129890e1cd7c3
Karkv <108710246+Karkv@users.noreply.github.com>
2023-09-15 11:13:26 +0530

    Initial commit

---
+++ plotdemo.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5,6]
+y=[2,4,6,8,10,12]
+y=[-x*x for x in x][::-1]
+plt.scatter(x,y)
+plt.plot(x,y)
+plt.grid(True)
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Second Degree Curve")
+plt.show()
